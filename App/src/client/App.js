72724876052e8e139dac94852d4df0b177454526
Ashika Balamurugan <ashikabala01@gmail.com>
2018-08-04 21:41:07 -0700

    fb auth implementation

--- App/src/client/App.js
+++ App/src/client/App.js
@@ -1,6 +1,18 @@
-import React, { Component } from "react";
-import "./app.css";
-import ReactImage from "./react.png";
+import React, { Component } from 'react';
+import './app.css';
+import {
+  Jumbotron,
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from 'reactstrap';
 
 export default class App extends Component {
   constructor(props) {
@@ -9,20 +21,54 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    fetch("/api/getUsername")
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    fetch('/api/facebook/auth').then((res) => {
+      console.log(res.json);
+      res.json();
+    });
+    // .then(user => this.setState({ username: user.username }));
   }
 
   render() {
     return (
       <div>
-        {this.state.username ? (
-          <h1>Hello {this.state.username}</h1>
-        ) : (
-          <h1>Loading.. please wait!</h1>
-        )}
-        <img src={ReactImage} alt="react" />
+        <Jumbotron>
+          <h1>
+            {' '}
+Hello Ashika! Good Morning! Have a Great day!
+          </h1>
+        </Jumbotron>
+
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">
+            {' '}
+Social Share
+            {' '}
+          </NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/">
+Sign In
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">
+Login
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+
+        <Form>
+          <FormGroup>
+            <Label for="content">
+Text Area
+            </Label>
+            <Input type="textarea" name="text" id="contentText" rows={5} />
+          </FormGroup>
+          <Button>
+Post
+          </Button>
+        </Form>
       </div>
     );
   }
--- App/src/server/index.js
+++ App/src/server/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
-const os = require('os');
 
 const app = express();
+const cors = require('cors');
+
+// const os = require('os');
+const request = require('request-promise');
+
+const OAuth2 = require('oauth').OAuth2;
+
+const oauth2 = new OAuth2(
+  '638447646525666', // App id
+  '232b2c67a028c78978f56776af3c9d32', // App secret
+  '',
+  'https://www.facebook.com/dialog/oauth',
+  'https://graph.facebook.com/oauth/access_token',
+  null
+);
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.use(cors({ credentials: true, origin: true }));
+
+app.get('/api/facebook/auth', (req, res) => {
+  const redirect_uri = 'http://localhost:8080/facebook/callback';
+  // For eg. "http://localhost:3000/facebook/callback"
+  const params = { redirect_uri, scope: 'default' };
+  console.log(params);
+  res.redirect(oauth2.getAuthorizeUrl(params));
+});
+
+// app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 app.listen(8080, () => console.log('Listening on port 8080!'));
